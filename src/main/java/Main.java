166d642960889e5d958eba55c6111f51c1dbdc25
Amax-23 <maksim.agoshkov@gmail.com>
2022-10-20 13:15:33 +0300

    Reformat code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,10 @@ import ru.netology.IntsCalculator;
 public class Main {
     public static void main(String[] args) {
         Ints intsCalc = new IntsCalculator();
-        System.out.println("Результат суммы: " + intsCalc.sum(2,2));
-        System.out.println("Результат умножения: " + intsCalc.mult(10,22));
-        System.out.println("Результат разности: " + intsCalc.sub(5,2));
-        System.out.println("Результат деления: " + intsCalc.div(10,2));
-        System.out.println("Результат возведения в степень: " + intsCalc.pow(2,10));
+        System.out.println("Результат суммы: " + intsCalc.sum(2, 2));
+        System.out.println("Результат умножения: " + intsCalc.mult(10, 22));
+        System.out.println("Результат разности: " + intsCalc.sub(5, 2));
+        System.out.println("Результат деления: " + intsCalc.div(10, 2));
+        System.out.println("Результат возведения в степень: " + intsCalc.pow(2, 10));
     }
 }
--- src/main/java/ru/netology/Ints.java
+++ src/main/java/ru/netology/Ints.java
@@ -2,8 +2,12 @@ package ru.netology;
 
 public interface Ints {
     int sum(int arg0, int arg1);
+
     int mult(int arg0, int arg1);
+
     int pow(int a, int b);
-    public int sub(int a, int b);
-    public int div(int a, int b);
+
+    int sub(int a, int b);
+
+    int div(int a, int b);
 }
