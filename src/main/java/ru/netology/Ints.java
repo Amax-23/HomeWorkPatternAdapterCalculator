a5cf2b6bc507ec2d0b766e8cf3587fe1729834a8
Amax-23 <maksim.agoshkov@gmail.com>
2022-10-20 13:05:40 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Ints.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+public interface Ints {
+    int sum(int arg0, int arg1);
+    int mult(int arg0, int arg1);
+    int pow(int a, int b);
+    public int sub(int a, int b);
+    public int div(int a, int b);
+}
